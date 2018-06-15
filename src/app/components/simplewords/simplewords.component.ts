70a0339a738785ad17efef17e331c48fe3fd5b3b
Donovan Venuto <tronovan@gmail.com>
2018-06-15 09:13:59 -0500

    adding dependencies

--- src/app/components/simplewords/simplewords.component.ts
+++ src/app/components/simplewords/simplewords.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { SimplewordsService } from '../../services/simplewords.service';
+';
 @Component({
   selector: 'app-simplewords',
   templateUrl: './simplewords.component.html',
