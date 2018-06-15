eed7d148d691eb7a8233528ecc9092356588e337
Donovan Venuto <tronovan@gmail.com>
2018-06-15 09:08:52 -0500

    creating simplewords component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { SimplewordsService } from './services/simplewords.service';
 import { AppComponent } from './app.component';
+import { SimplewordsComponent } from './components/simplewords/simplewords.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SimplewordsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/simplewords/simplewords.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-simplewords',
+  templateUrl: './simplewords.component.html',
+  styleUrls: ['./simplewords.component.css']
+})
+export class SimplewordsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
