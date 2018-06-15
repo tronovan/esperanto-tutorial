86954bde9c3ee1601b6a2b22749e16a06b6fc23e
Donovan Venuto <tronovan@gmail.com>
2018-06-15 09:05:38 -0500

    Adding dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { SimplewordsService } from './services/simplewords.service';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +10,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [SimplewordsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
